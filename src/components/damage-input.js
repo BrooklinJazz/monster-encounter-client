3824e7a57c308f8218ebbd708708fff158c028a3
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-01-18 11:24:59 -0800

    show and hide current hp and damage input on click

---
+++ src/components/damage-input.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+
+export default class DamageInput extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+      </form>
+    )
+  }
+}
