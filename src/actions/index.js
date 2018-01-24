d9e3b3fd574d8690c117d5836b0e75c0ff4e5880
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-01-23 18:28:24 -0800

    more refactoring and addition of notes in monster_reducer and actions index

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,20 +12,21 @@ const createAction = (type, params = null) => ({ type, ...params });
 //   };
 // }
 
-export const selectCombatant = monster =>
-  createAction(Types.MONSTER_SELECTED, { monster });
+// Actions relating to MonsterList
+export const addMonsterToCombatants = monster =>
+createAction(Types.ADD_MONSTER_TO_COMBATANTS, { monster });
+
+export const filterLibrary = searchTerm => {
+  console.log('in actions',searchTerm)
+  return createAction(Types.FILTER_MONSTER_LIBRARY, { searchTerm });
+}
+
+// Actions relating to CombatantList
+export const selectCombatant = combatant =>
+  createAction(Types.MONSTER_SELECTED, { combatant });
 
 export const changeCombatantHp = payload =>
   createAction(Types.CHANGE_MONSTER_HP, { payload });
 
 export const removeCombatant = payload =>
   createAction(Types.REMOVE_COMBATANT, { payload })
-
-export const addMonsterToCombatants = monster =>
-  createAction(Types.ADD_MONSTER_TO_COMBATANTS, { monster });
-
-
-export const filterLibrary = searchTerm => {
-  console.log('in actions',searchTerm)
-  return createAction(Types.FILTER_MONSTER_LIBRARY, { searchTerm });
-}
--- src/reducers/monsters_reducer.js
+++ src/reducers/monsters_reducer.js
@@ -1,16 +1,13 @@
 // state argument is not application state, only the state
-// this reducer is responsible for
-// NOTE TODO selecting one monster to start, but may have several active
-// monsters at once
+// this reducer is responsible for tasks relating to monsters
 import monstersData from "../data/monsters";
 import Types from "../types";
 import CombatantList from "../containers/CombatantList";
 import helpers from "../../helpers"
+// the array of monster objects exported as a function.
+// NOTE storing monsters in a local file currently
 const monsters = monstersData()
 
-// most of the time people actually use an immutable data structure for reducers
-// something like: https://github.com/rtfeldman/seamless-immutable
-// or: https://github.com/facebook/immutable-js
 const INITIAL_STATE = {
   monsters,
   selectedMonster: null,
@@ -20,33 +17,44 @@ const INITIAL_STATE = {
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
+    // Select combatant to show detailed stats
+    // coming from Combatant.js
+    /****************************************
+                MonsterList
+    ****************************************/
     case Types.MONSTER_SELECTED:
-      return { ...state, selectedMonster: action.monster };
+      return { ...state, selectedMonster: action.combatant };
+      // add a Monster obj to the CombatantList
     case Types.ADD_MONSTER_TO_COMBATANTS:
-      const newMonster = helpers.deepClone(action.monster);
-      newMonster.currentHp = newMonster.HP.Value
+      const newCombatant = helpers.deepClone(action.monster);
+      newCombatant.currentHp = newCombatant.HP.Value
       const newCombatantList = state.CombatantList.concat(
-        newMonster,
-        // action.monster,
+        newCombatant
       );
       return {
         ...state,
-        CombatantList: newCombatantList,
+        CombatantList: newCombatantList
       };
+      case Types.FILTER_MONSTER_LIBRARY:
+      return {
+        ...state,
+        searchTerm: action.searchTerm
+      };
+    /***************************************
+                CombatantList
+    /**************************************/
       case Types.REMOVE_COMBATANT:
-      // const combatantsListAfterRemove = state.CombatantList.splice(action.payload.index, 1)
+      // assign a constant to be equal to CombatantList.
+      // using map to avoid mutating state.
       const combatantsListAfterRemove = state.CombatantList.map( (combatant, i) => {
         return combatant
       })
+      // remove the combatant at the index given by the action
       combatantsListAfterRemove.splice(action.payload.index, 1)
+      // return the CombatantList with the desired combatant removed
         return {
           ...state,
           CombatantList: combatantsListAfterRemove
-        };
-      case Types.FILTER_MONSTER_LIBRARY:
-        return {
-          ...state,
-          searchTerm: action.searchTerm
         };
       case Types.CHANGE_MONSTER_HP:
         const combatantsListAfterChange = state.CombatantList.map( (combatant, i) => {
