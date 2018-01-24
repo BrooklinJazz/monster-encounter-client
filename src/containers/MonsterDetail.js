1119caab57222ad216070f449dd789d622e76114
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-01-23 20:27:41 -0800

    set up MonsterDetail with divs for all values needed. add text to the monster stats that are not arrays

--- src/containers/MonsterDetail.js
+++ src/containers/MonsterDetail.js
@@ -28,6 +28,7 @@ class MonsterDetail extends Component {
 
 
       // an arr of obj
+      // Saves (only the ones with extra modifiers)
       Saves = [],
       Skills = [],
       Traits = [],
@@ -45,21 +46,94 @@ class MonsterDetail extends Component {
       Wis = 0,
       Cha = 0,
     } = monster.Abilities
-    // Saves (only the ones with extra modifiers)
-
+    // const SpeedPrettier = Speed.map( value =>
+    //   return (
+    //     value.concat(' ')
+    //   )
+    // )
     // console.log(InitiativeModifier);
     return (
-      <div>
-        <h3>Details for:</h3>
-        <div>Name: {Name}</div>
-        <div>Source: {Source}</div>
-        <div>Type: {Type}</div>
-        <div onClick={() => console.log('MonsterDetail.js', monster.HP.Value)}>Hp: {monster.HP.Value}</div>
-        <div>AC: {monster.AC.Value}</div>
-        <div>
-          Speed: {monster.speed}
+      <div class="monsterDetail">
+        {/* Top with AC, HP, Speed */}
+        <div className="statRow">
+          <strong className="statName">Armor Class</strong>
+          <div className="statValue">{AC.Value} {AC.Notes}</div>
         </div>
-        <hr/>
+        <div className="statRow">
+          <strong className="statName">Hit Points</strong>
+          <div className="statValue">{HP.Value} {HP.Notes}</div>
+        </div>
+        <div className="statRow">
+          <strong className="statName">Speed</strong>
+          <div className="statValue">{Speed}</div>
+        </div>
+
+        <div className="Saves"></div>
+
+        <div className="Skills"></div>
+
+        <div className="Senses"></div>
+
+        <div class="abilities">
+          <div className="abilityContainer">
+            <strong className="abilityName">STR</strong>
+            <div className="abilityValue">{Str}</div>
+          </div>
+          <div className="abilityContainer">
+            <strong className="abilityName">DEX</strong>
+            <div className="abilityValue">{Dex}</div>
+          </div>
+          <div className="abilityContainer">
+            <strong className="abilityName">CON</strong>
+            <div className="abilityValue">{Con}</div>
+          </div>
+          <div className="abilityContainer">
+            <strong className="abilityName">INT</strong>
+            <div className="abilityValue">{Int}</div>
+          </div>
+          <div className="abilityContainer">
+            <strong className="abilityName">WIS</strong>
+            <div className="abilityValue">{Wis}</div>
+          </div>
+          <div className="abilityContainer">
+            <strong className="abilityName">CHA</strong>
+            <div className="abilityValue">{Cha}</div>
+          </div>
+        </div>
+
+        <div className="statRow">
+          <strong className="statName">Damage Immunities</strong>
+          <div className="statValue">{DamageImmunities}</div>
+        </div>
+        <div className="statRow">
+          <strong className="statName">Condition Immunities</strong>
+          <div className="statValue">{ConditionImmunities}</div>
+        </div>
+        <div className="statRow">
+          <strong className="statName">Senses</strong>
+          <div className="statValue">{Senses}</div>
+        </div>
+        <div className="statRow">
+          <strong className="statName">Languages</strong>
+          <div className="statValue">{Languages}</div>
+        </div>
+        <div className="statRow">
+          <strong className="statName">Challenge</strong>
+          <div className="statValue">{Challenge}</div>
+        </div>
+        <div className="statRow">
+          <strong className="statName"></strong>
+          <div className="statValue"></div>
+        </div>
+
+        <div className="traits"></div>
+
+        <div className="actions"></div>
+
+        <div className="legendaryActions"></div>
+
+
+
       </div>
     );
   }
