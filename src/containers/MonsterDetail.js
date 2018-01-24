4866af7972705abd60f47a5488f915aff569f9ff
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-01-23 19:51:11 -0800

    define monster variables for MonsterDetail

--- src/containers/MonsterDetail.js
+++ src/containers/MonsterDetail.js
@@ -3,23 +3,69 @@ import {connect} from "react-redux";
 
 class MonsterDetail extends Component {
   render() {
-    if (!this.props.monster) {
+    const {monster = {}} = this.props
+
+    if (!monster) {
       return <div>Select a monster to get started</div>;
     }
+    // this must be below if statement or it will be defined when monster is empty
+    // NOTE InitiativeModifier is always 0 in JSON?
+    const {
+      Name = '',
+      Source = '',
+      Type = '',
+      Challenge = '',
+      HP = [],
+      AC = [],
+      Speed = [],
+      DamageVulnerabilities = [],
+      DamageResistances = [],
+      DamageImmunities = [],
+      ConditionImmunities = [],
+      Senses = [],
+      Languages = [],
 
-    return (<div>
-      <h3>Details for:</h3>
-      <div>Name: {this.props.monster.Name}</div>
-      <div>Source: {this.props.monster.Source}</div>
-      <div>Type: {this.props.monster.Type}</div>
-      <div onClick={() => console.log('MonsterDetail.js', this.props.monster.HP.Value)}>Hp: {this.props.monster.HP.Value}</div>
-      <div>AC: {this.props.monster.AC.Value}</div>
-      <div>Speed: {this.props.monster.Speed}</div>
-      <hr/>
-    </div>);
+
+
+      // an arr of obj
+      Saves = [],
+      Skills = [],
+      Traits = [],
+      Actions = [],
+      Reactions = [],
+      LegendaryActions = [],
+
+    } = monster
+    // Abilites
+    const {
+      Str = 0,
+      Dex = 0,
+      Con = 0,
+      Int = 0,
+      Wis = 0,
+      Cha = 0,
+    } = monster.Abilities
+    // Saves (only the ones with extra modifiers)
+
+    // console.log(InitiativeModifier);
+    return (
+      <div>
+        <h3>Details for:</h3>
+        <div>Name: {Name}</div>
+        <div>Source: {Source}</div>
+        <div>Type: {Type}</div>
+        <div onClick={() => console.log('MonsterDetail.js', monster.HP.Value)}>Hp: {monster.HP.Value}</div>
+        <div>AC: {monster.AC.Value}</div>
+        <div>
+          Speed: {monster.speed}
+        </div>
+        <hr/>
+      </div>
+    );
   }
 }
 
+// get selectedMonster from state and name as monster
 function mapStateToProps(state) {
   const {selectedMonster: monster} = state.monsters;
   return {monster};
