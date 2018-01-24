4c85598450a211d2c6bc6927f04bbf3eaf537f9f
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-01-23 17:59:14 -0800

    basic styling for combatants

--- src/containers/CombatantList.js
+++ src/containers/CombatantList.js
@@ -30,86 +30,45 @@ class CombatantList extends Component {
       console.log('combatant combatants state:', this.state);
       return (
         <div>
-
-          <Combatant combatant={combatant} index={index}/>
-          {/* <Combatant /> */}
-          <li key={index}
-            // onClick={() => this.props.selectCombatant(combatant)}
-            className="list-group-item">
-            {combatant.Name}
-            <div>
-              {
-                this.state.showComponent
-                ? <form onClick={(e) => e.stopPropagation()} onSubmit={(e) => {
-                  e.preventDefault()
-                  this.props.changeCombatantHp({combatant, hpChange: this.state.hpChange, index: index})
-                  this.setState({showComponent: false});
-                }
-              }>
-              <input type="text" autoFocus name="hpChange" onChange={(e) => this.handleChange(e)}/>
-            </form>
-            : <div onClick={(e) => {
-              this._onButtonClick(e)
-              this.focusTextInput(this.textInput)
-            }}>
-            CURRENT HP: {combatant.currentHp}
-          </div>
-        }
-      </div>
-
-      <div>
-        MAX HP: {combatant.HP.Value}
-      </div>
-      <div>
-        AC: {combatant.AC.Value}
-      </div>
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          this.props.removeCombatant({combatant, index: index})
-        }
-      }>Delete</button>
-
-    </li>
-  </div>
-  )
-
-}
-
-);
-}
-//NOTE render only Combatant
-
-
-// TODO put logic for changeCombatantHp in monster_reducer here.
-// function used for handling changeCombatantHp with form input
-handleChange(event) {
-  const newState = Object.assign({}, this.state, {
-    [event.target.name]: event.target.value
-  });
-  this.setState(newState)
-}
-
-focusTextInput(a) {
-  // Explicitly focus the text input using the raw DOM API
-  console.log('this', this);
-  console.log('this.textInput', this.textInput);
-  this.textInput.focus();
-}
-
-_onButtonClick(e) {
-  e.stopPropagation()
-  this.setState({showComponent: true});
-}
-
-render() {
-  if (!this.props.CombatantList) {
-    return <div>Select a CombatantList to get started</div>;
+          <li className="list-group-item">
+            <Combatant combatant={combatant} index={index}/>
+          </li>
+        </div>
+      )
+    });
   }
-  // CombatantListArr.push(this.props.CombatantList);
-  // console.log(CombatantListArr);
-  return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
-}
+  //NOTE render only Combatant
+
+
+  // TODO put logic for changeCombatantHp in monster_reducer here.
+  // function used for handling changeCombatantHp with form input
+  handleChange(event) {
+    const newState = Object.assign({}, this.state, {
+      [event.target.name]: event.target.value
+    });
+    this.setState(newState)
+  }
+
+  focusTextInput(a) {
+    // Explicitly focus the text input using the raw DOM API
+    console.log('this', this);
+    console.log('this.textInput', this.textInput);
+    this.textInput.focus();
+  }
+
+  _onButtonClick(e) {
+    e.stopPropagation()
+    this.setState({showComponent: true});
+  }
+
+  render() {
+    if (!this.props.CombatantList) {
+      return <div>Select a CombatantList to get started</div>;
+    }
+    // CombatantListArr.push(this.props.CombatantList);
+    // console.log(CombatantListArr);
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+  }
 }
 
 // anything in mapStateToProps will be this.props in the container above.
