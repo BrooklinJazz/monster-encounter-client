a7705f07e9290a7e47403278251dcf411c8beeb3
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-01-25 11:39:41 -0800

    clicking ability scores now creates a Roll component

--- helpers.js
+++ helpers.js
@@ -12,3 +12,9 @@ export const d20 = () => {
   const result = Math.floor(Math.random() * 20) + 1
   return result
 }
+// take an Ability Score to it's modifier i.e 12 becomes 1 or 26 becomes 8.
+// always subtract 10 from n and divide by 2. always round down.
+export const convScoreToMod  = (n) => {
+  const result = Math.floor( (n - 10) / 2)
+  return result
+}
