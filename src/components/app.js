6f9966defe444cb08898f8e74bc477743c756aa1
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-01-26 12:32:38 -0800

    demo setting local state with fetch request in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,29 @@ import MonsterDetail from "../containers/MonsterDetail";
 import CombatantList from "../containers/CombatantList";
 import Rolls from "../containers/Rolls";
 
+const testUrl = 'http://www.dnd5eapi.co/api/monsters/1'
+const serverUrl = 'http://localhost:3002'
+
 export default class App extends Component {
+  state = {monsterArray: []}
+
+  componentDidMount() {
+    // console.log('App Component Did Mount');
+    // console.log('http://dnd5eapi.co/api/monsters/');
+    fetch(
+      serverUrl,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }
+    )
+    .then(res => res.json())
+    // .then(res => console.log(res)) // this will bug
+    .then(res => this.setState({ monsterArray: res }))
+  }
+
   render() {
     return (
         <div className="row">
